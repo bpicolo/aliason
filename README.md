16f2c4ade907a60be15d3a77ce3cd0c1af6d6fea
Ben Picolo <ben@Bens-MacBook-Pro.local>
2017-04-17 17:58:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# aliason
+
+aliason is a shell tool for managing project-specific shell aliases.
+
+I created aliason primarily out of the desire to not remember specific test-running
+commands for different repos. Task runners like Make have left
+me shorthanded because passing arguments can be such a pain, whereas shell aliases
+have been useful, but not portable between projects.
+
+## Using aliason
+Getting started with aliason is straightforward, simply:
+``bash
+aliason install >> ~/.bash_profile  # or bashrc/other shell source file of choice
+```
+and then source that file.
+
+This does two things:
+1. adds a `cd` function that, as an alternative to cd, will cd followed by `eval $(aliason env)`
+2. adds an `eval $(aliason env)` directly, so creating new shells will also source your env.
+
+These options seemed the most straightforward, but there a couple alternatives. Manually
+running `eval $(aliason env)` should pick up the aliases in your current env. There's also
+a valid path through PROMPT_COMMAND, and probably any number of other alternatives as well.
+Anything that runs `eval $(aliason env)` when desired should do the trick.
+
+## Declaring aliases
+aliason will look for and .aliasrc file in your current directory. An .aliasrc file is a
+simple mapping of alias names to commands in yaml-syntax.
+
+```yaml
+ping: pong
+test: tox
+```
+
+## todo:
+1. Support preserving global aliases when moving between directories (or also support not-overwriting?)
+2. Tests
---
+++ aliason.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"fmt"
+
+	"github.com/urfave/cli"
+	"gopkg.in/yaml.v2"
+)
+
+const aliasFilename = ".aliasrc"
+const unaliasEnvVar = "ALIASION_UNENV"
+const aliasonInstall = `
+function cd() {
+    new_directory="$*";
+    if [ $# -eq 0 ]; then
+        new_directory=${HOME};
+    fi;
+    builtin cd "${new_directory}" && eval $(aliason env)
+}
+eval $(aliason env)`
+
+func getRemoveCurrentAliases() string {
+	return os.Getenv(unaliasEnvVar)
+}
+
+func generateAliasCommand(m *map[string]string) string {
+	if len(*m) == 0 {
+		return ""
+	}
+
+	var parts []string
+	for k, v := range *m {
+		parts = append(parts, fmt.Sprintf("%s=\"%s\"", k, v))
+	}
+
+	return fmt.Sprintf("alias %s", strings.Join(parts, " "))
+}
+
+func generateUnaliasCommand(m *map[string]string) string {
+	var keys []string
+	for k := range *m {
+		keys = append(keys, k)
+	}
+
+	return fmt.Sprintf("export %s=\"unalias %s\"", unaliasEnvVar, strings.Join(keys, " "))
+}
+
+func unenvAliason() *[]string {
+	var commands []string
+	if command := getRemoveCurrentAliases(); command != "" {
+		commands = append(commands, command)
+	}
+	commands = append(commands, fmt.Sprintf("unset %s", unaliasEnvVar))
+
+	return &commands
+}
+
+func sourceAliasrc() *[]string {
+	var commands []string
+	if command := getRemoveCurrentAliases(); command != "" {
+		commands = append(commands, command)
+	}
+
+	if _, err := os.Stat(aliasFilename); os.IsNotExist(err) {
+		commands = append(commands, fmt.Sprintf("unset %s", unaliasEnvVar))
+		return &commands
+	}
+
+	data, err := ioutil.ReadFile(aliasFilename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to load .aliasrc")
+	}
+
+	m := make(map[string]string)
+	err = yaml.Unmarshal(data, &m)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to parse .aliasrc file.")
+	}
+
+	if command := generateAliasCommand(&m); command != "" {
+		commands = append(commands, command)
+	}
+
+	if command := generateUnaliasCommand(&m); command != "" {
+		commands = append(commands, command)
+	}
+
+	return &commands
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "aliason"
+	app.Usage = "Easily manage project-specific shell aliases"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "env",
+			Usage: "Source .aliasrc in the current directory.",
+			Action: func(c *cli.Context) error {
+				commands := sourceAliasrc()
+				if len(*commands) > 0 {
+					fmt.Println(strings.Join(*commands, " &&\n "))
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "unenv",
+			Usage: "Unsource the current aliason env.",
+			Action: func(c *cli.Context) error {
+				commands := unenvAliason()
+				if len(*commands) > 0 {
+					fmt.Println(strings.Join(*commands, " ;\n "))
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "install",
+			Usage: "Generate useful bashrc handlers for enabling aliason",
+			Action: func(c *cli.Context) error {
+				fmt.Println(aliasonInstall)
+				return nil
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
